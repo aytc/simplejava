04159625a28d564c6cc918696b78b5289bfd9964
aytac.akkaya <aytac.akkaya@muhasebat.gov.tr>
2018-10-10 17:26:22 +0300

    Erişim belirleyicileri hakkında bir örnekleme yapıldı.

---
+++ src/basic2/Beverage.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basic2;
+
+/**
+ *
+ * @author aytacakkaya
+ */
+/*varsayılan erişim belirleyicisine sahip olduğu için diğer paketler tarafından erişilemeyecektir. default access modifier paketler düzeyinde erişim sağlamaktadır.*/
+class Beverage {
+
+}
